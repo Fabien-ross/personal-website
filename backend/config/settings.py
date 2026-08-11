a7c057db90131e2c0482940d59b20b1d6314210f
Fabien-ross <fabienrosset0@gmail.com>
2026-08-11 15:06:15 +0000

    updated logging django to debug

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,3 +142,22 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# LOGGING
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
